6176f1f5a08093720c6fb283158f0f470717653b
Kudryavceva Irina <kosatkalastochka@gmail.com>
2025-11-15 18:10:52 +0300

    BoxTest  с валидацией

--- src/test/java/ru/job4j/BoxTest.java
+++ src/test/java/ru/job4j/BoxTest.java
@@ -8,76 +8,79 @@ import static org.assertj.core.api.Assertions.*;
 
 class BoxTest {
     @Test
-    void wenWhatsThisSphere(){
-        Box box= new Box(0,4);
-        String result =box.whatsThis();
+    void wenWhatsThisSphere() {
+        Box box = new Box(0, 4);
+        String result = box.whatsThis();
         assertThat(result).isNotEmpty()
                 .doesNotContain("figure")
                 .isEqualTo("Sphere");
     }
 
     @Test
-    void wenWhatsThisTetrahedron(){
-        Box box= new Box(4,7);
-        String result =box.whatsThis();
+    void wenWhatsThisTetrahedron() {
+        Box box = new Box(4, 7);
+        String result = box.whatsThis();
         assertThat(result).isNotEmpty()
                 .doesNotContain("figure")
                 .isEqualTo("Tetrahedron");
     }
 
     @Test
-    void wenWhatsThisCube(){
-        Box box= new Box(8,7);
-        String result =box.whatsThis();
+    void wenWhatsThisCube() {
+        Box box = new Box(8, 7);
+        String result = box.whatsThis();
         assertThat(result).isNotEmpty()
                 .doesNotContain("figure")
                 .isEqualTo("Cube");
     }
 
     @Test
-    void wenWhatsThisUNKNOWN(){
-        Box box= new Box(10,7);
-        String result =box.whatsThis();
+    void wenWhatsThisUNKNOWN() {
+        Box box = new Box(10, 7);
+        String result = box.whatsThis();
         assertThat(result).isNotEmpty()
                 .endsWith("object")
                 .isEqualTo("Unknown object");
     }
+
     @Test
-    void WenInitEdgeNegative(){
-        Box box= new Box(0,-7);
+    void wenInitEdgeNegative() {
+        Box box = new Box(0, -7);
         String result = box.whatsThis();
         assertThat(result).isNotEmpty()
                 .startsWithIgnoringCase("unknown")
                 .isEqualTo("Unknown object");
     }
+
      @Test
-    void getNumberOfVerticesWenExist(){
-        Box box = new Box(8,3);
+    void getNumberOfVerticesWenExist() {
+        Box box = new Box(8, 3);
         int result = box.getNumberOfVertices();
         assertThat(result).isPositive()
                 .isGreaterThan(-1)
                 .isLessThan(9)
-                .isIn(0,4,8);
-    }
-    @Test
-    void getNumberOfVerticesWenNotExist(){
-        Box box = new Box(10,3);
-        int result = box.getNumberOfVertices();
-        assertThat(result).isNegative()
-                .isOdd()
-                .isIn(-1,-2);
+                .isIn(0, 4, 8);
     }
 
     @Test
-    void isExistWenExist(){
-        Box box = new Box(0,4);
+    void getNumberOfVerticesWenNotExist() {
+        Box box = new Box(10, 3);
+        int result = box.getNumberOfVertices();
+        assertThat(result).isNegative()
+                .isOdd()
+                .isIn(-1, -2);
+    }
+
+    @Test
+    void isExistWenExist() {
+        Box box = new Box(0, 4);
         boolean result = box.isExist();
         assertThat(result).isTrue();
     }
 
     @Test
-    void isExistWenNotExist(){
-        Box box = new Box(-1,4);
+    void isExistWenNotExist() {
+        Box box = new Box(-1, 4);
         boolean result = box.isExist();
         assertThat(result).isFalse();
     }
@@ -85,36 +88,36 @@ class BoxTest {
     @Test
     void  getAreaWenSphere() {
         double a = 4.0;
-        Box box = new Box(0,(int) a);
+        Box box = new Box(0, (int) a);
         double result = box.getArea();
         assertThat(result).isPositive()
-                .isGreaterThan(4*a*a)
-                .isLessThan(16*a*a)
+                .isGreaterThan(4 * a * a)
+                .isLessThan(16 * a * a)
                 .isEqualTo(4 * Math.PI * (a * a));
     }
 
     @Test
-    void  getAreaWenTetrahedron(){
+    void  getAreaWenTetrahedron() {
         double a = 4.0;
-        Box box = new Box(4,(int) 4);
+        Box box = new Box(4, (int) 4);
         double result = box.getArea();
         assertThat(result).isPositive()
-                .isGreaterThan(a*a)
-                .isLessThan(3*a*a)
+                .isGreaterThan(a * a)
+                .isLessThan(3 * a * a)
                 .isEqualTo(Math.sqrt(3) * (a * a));
     }
 
     @Test
-    void  getAreaWenCube(){
+    void  getAreaWenCube() {
         double a = 4.0;
-        Box box = new Box(8,(int) 4);
+        Box box = new Box(8, (int) 4);
         double result = box.getArea();
         assertThat(result).isPositive()
                 .isEqualTo(6 * (a * a));
     }
 
     @Test
-    void  getAreaWenNotExist(){
+    void  getAreaWenNotExist() {
         Box box = new Box(12, 4);
         double result = box.getArea();
         assertThat(result) .isGreaterThan(-20.0)

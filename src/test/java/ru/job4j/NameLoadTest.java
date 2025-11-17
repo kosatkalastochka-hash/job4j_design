7166078be709bfe4b5aecfe93eee4ef7d14dad95
Kudryavceva Irina <kosatkalastochka@gmail.com>
2025-11-17 17:57:39 +0300

    NameLoadTest

---
+++ src/test/java/ru/job4j/NameLoadTest.java
@@ -0,0 +1,54 @@
+package ru.job4j;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class NameLoadTest {
+    @Test
+    void checkEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::getMap)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("no data");
+    }
+
+    @Test
+    void parseWenNamesEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::parse)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("array is empty");
+
+    }
+
+    @Test
+    void  validateWenNameNotContainsEquals() {
+        NameLoad nameLoad = new NameLoad();
+        String name = "дерево берёза";
+        assertThatThrownBy(() -> nameLoad.parse(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(name)
+                .hasMessageContaining("does not contain the symbol '='");
+    }
+
+    @Test
+    void  validateWenNameStartsWithEquals() {
+        NameLoad nameLoad = new NameLoad();
+        String name = "= берёза";
+        assertThatThrownBy(() -> nameLoad.parse(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(name)
+                .hasMessageContaining("does not contain a key");
+    }
+
+    @Test
+    void  validateWenNameEndsWithEquals() {
+        NameLoad nameLoad = new NameLoad();
+        String name = "дерево =";
+        assertThatThrownBy(() -> nameLoad.parse(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(name)
+                .hasMessageContaining("does not contain a value");
+    }
+}

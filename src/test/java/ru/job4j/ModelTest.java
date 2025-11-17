775855fc7773774f570d7a246c25f5784be04c7e
Kudryavceva Irina <kosatkalastochka@gmail.com>
2025-11-17 13:09:59 +0300

    ModelTest с валидацией

--- src/test/java/ru/job4j/ModelTest.java
+++ src/test/java/ru/job4j/ModelTest.java
@@ -8,12 +8,14 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 class ModelTest {
+
     @Test
     void checkBoolean() {
         Model model = new Model(1, 5.255d, "name", true);
         boolean result = model.isCondition();
         assertThat(result).isTrue();
     }
+
     @Test
     void checkStringChain() {
         Model model = new Model(1, 5.255d, "I am learning Java", true);
@@ -29,6 +31,7 @@ class ModelTest {
                 .endsWith("Java")
                 .isEqualTo("I am learning Java");
     }
+
     @Test
     void checkInt() {
         Model model = new Model(2, 5.2d, "name", true);
@@ -40,6 +43,7 @@ class ModelTest {
                 .isLessThan(3)
                 .isEqualTo(2);
     }
+
     @Test
     void checkDoubleChain() {
         Model model = new Model(1, 5.255d, "name", true);

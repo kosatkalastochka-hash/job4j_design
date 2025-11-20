1963660f42705d862f9eaae8e2e1a2cc8d9c43df
Kudryavceva Irina <kosatkalastochka@gmail.com>
2025-11-20 12:19:29 +0300

    EvenNumbersIterator исправленный

--- src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
+++ src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
@@ -1,32 +1,25 @@
 package ru.job4j.iterator;
 
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 import java.util.NoSuchElementException;
 
 public class EvenNumbersIterator implements Iterator<Integer> {
 
     private final int[] data;
     private int index;
-    private final List<Integer> indices = new ArrayList<>();
 
     public EvenNumbersIterator(int[] data) {
         this.data = data;
-        initializationIndices();
-    }
-
-    private void initializationIndices() {
-        for (int i = 0; i <= data.length - 1; i++) {
-            if (data[i] % 2 == 0) {
-                indices.add(i);
-            }
-        }
     }
 
     @Override
     public boolean hasNext() {
-        return index < indices.size();
+        for (; index <= data.length - 1; index++) {
+            if (data[index] % 2 == 0) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
@@ -34,7 +27,6 @@ public class EvenNumbersIterator implements Iterator<Integer> {
         if (!hasNext()) {
             throw new NoSuchElementException();
         }
-
-        return data[indices.get(index++)];
+        return data[index++];
     }
 }

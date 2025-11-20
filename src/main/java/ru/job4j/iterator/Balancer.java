a78a023262ee21fc8181221df837726a89798813
Kudryavceva Irina <kosatkalastochka@gmail.com>
2025-11-20 16:27:12 +0300

    Balancer

---
+++ src/main/java/ru/job4j/iterator/Balancer.java
@@ -0,0 +1,16 @@
+package ru.job4j.iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Balancer {
+
+    public static void split(List<ArrayList<Integer>> nodes, Iterator<Integer> source) {
+        while (source.hasNext()) {
+            for (int i = 0; i < nodes.size() && source.hasNext(); i++) {
+                nodes.get(i).add(source.next());
+            }
+        }
+    }
+}

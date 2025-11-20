89265c0da0fab02fcd0fe482444a987f57cd6324
Kudryavceva Irina <kosatkalastochka@gmail.com>
2025-11-20 13:00:14 +0300

    NonNullIterator исправленный

---
+++ src/main/java/ru/job4j/iterator/NonNullIterator.java
@@ -0,0 +1,32 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class NonNullIterator implements Iterator<Integer> {
+
+    private final Integer[] data;
+    private int index;
+
+    public NonNullIterator(Integer[] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        for (; index <= data.length - 1; index++) {
+            if (data[index] != null) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data[index++];
+    }
+}

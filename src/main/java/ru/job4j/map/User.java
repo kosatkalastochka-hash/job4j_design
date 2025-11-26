3cd22c9a9950a81df4b40e4b6a7e053311efccb0
Kudryavceva Irina <kosatkalastochka@gmail.com>
2025-11-26 17:58:58 +0300

    User  исправлен конструктор

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,14 +1,15 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class User {
-   private String name;
-   private int children;
-   private Calendar birthday;
+    private String name;
+    private int children;
+    private Calendar birthday;
 
-    public User(Calendar birthday, int children, String name) {
-        this.birthday = birthday;
+    public User(int birthdayYear, int birthdayMonth, int birthdayDay, int children, String name) {
+        this.birthday = new GregorianCalendar(birthdayYear, birthdayMonth, birthdayDay);
         this.children = children;
         this.name = name;
     }

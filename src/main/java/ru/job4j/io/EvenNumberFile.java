c9e10121a899105a36f47bec6913d37fd1cf087e
Kudryavceva Irina <kosatkalastochka@gmail.com>
2025-12-14 21:58:08 +0300

    EvenNumberFile

---
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -0,0 +1,23 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        StringBuilder text = new StringBuilder();
+        try (FileInputStream input = new FileInputStream(".\\src\\data\\even.txt")) {
+            int read;
+            while ((read = input.read()) != -1) {
+                text.append((char) read);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Arrays.stream(text.toString().split(System.lineSeparator()))
+                .map(Integer::valueOf)
+                .filter(integer -> integer % 2 == 0)
+                .forEach(System.out::println);
+    }
+}
---
+++ src/main/java/ru/job4j/io/ReadFile.java
@@ -0,0 +1,23 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        StringBuilder text = new StringBuilder();
+        try (FileInputStream input = new FileInputStream(".\\src\\data\\ input.txt")) {
+            int read;
+            while ((read = input.read()) != -1) {
+                text.append((char) read);
+            }
+            System.out.println(text);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String[] lines = text.toString().split(System.lineSeparator());
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}

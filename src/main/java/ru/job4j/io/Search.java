67269af619dd96138eaccf4230d47b13a15f1cd7
Kudryavceva Irina <kosatkalastochka@gmail.com>
2025-12-23 12:17:27 +0300

    Валидация параметров запуска

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,16 +1,34 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.function.Predicate;
 
 public class Search {
+    private static Path start;
+
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, path -> path.toFile().getName().endsWith(".class")).forEach(System.out::println);
+        validate(args);
+        search(start, path -> path.toFile().getName().endsWith(args[1])).forEach(System.out::println);
+    }
+
+    private static void validate(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Неверное количество аргументов!. "
+                    + "Укажите 2 аргумента :директорию в котрой осуществляется поиск; "
+                    + "фильтр - символы которыми заканчивается название файла!");
+        }
+        start = Path.of(args[0]);
+        File file = start.toFile();
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {

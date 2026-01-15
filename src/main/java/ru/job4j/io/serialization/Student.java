277987087553c9533035b4b64bf7d4e137fb560a
Kudryavceva Irina <kosatkalastochka@gmail.com>
2026-01-15 14:38:45 +0300

    Преобразование JSON в POJO. JsonObject

--- src/main/java/ru/job4j/io/serialization/Student.java
+++ src/main/java/ru/job4j/io/serialization/Student.java
@@ -1,29 +1,17 @@
 package ru.job4j.io.serialization;
 
-import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBException;
-import jakarta.xml.bind.Marshaller;
-import jakarta.xml.bind.Unmarshaller;
-import jakarta.xml.bind.annotation.*;
+import org.json.JSONObject;
 
 import java.io.IOException;
-import java.io.StringReader;
-import java.io.StringWriter;
 import java.util.Arrays;
 
-@XmlRootElement(name = "student")
-@XmlAccessorType(XmlAccessType.FIELD)
 public class Student {
-    @XmlAttribute
+
     String name;
-    @XmlAttribute
     boolean served;
-    @XmlAttribute
     int age;
-    @XmlElement(name = "contact")
     Contact contact;
-    @XmlElementWrapper(name = "books")
-    @XmlElement(name = "book")
     String[] books;
 
     public Student() {
@@ -35,6 +23,26 @@ public class Student {
         this.name = name;
         this.served = served;
         this.books = books;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String[] getBooks() {
+        return books;
+    }
+
+    public Contact getContact() {
+        return contact;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isServed() {
+        return served;
     }
 
     @Override
@@ -53,20 +61,16 @@ public class Student {
                 new Contact(4569, "+7-996-532-34-98"),
                 "Андрей", true,
                 new String[]{"Биология", "Геометрия", "Химия"});
-        JAXBContext context = JAXBContext.newInstance(Student.class);
-        Marshaller marshaller = context.createMarshaller();
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-        String xml = "";
-        try (StringWriter writer = new StringWriter()) {
-            marshaller.marshal(student, writer);
-            xml = writer.getBuffer().toString();
-            System.out.println(xml);
-        }
-        Unmarshaller unmarshaller = context.createUnmarshaller();
-        try (StringReader reader = new StringReader(xml)) {
-            Student result = (Student) unmarshaller.unmarshal(reader);
-            System.out.println(result);
-        }
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("age", student.getAge());
+        jsonObject.put("contact", student.getContact());
+        jsonObject.put("name", student.getName());
+        jsonObject.put("served", student.isServed());
+        jsonObject.put("books", student.getBooks());
+        System.out.println(jsonObject.toString());
+
+        JSONObject jsonObject1 = new JSONObject(student);
+        System.out.println(jsonObject1.toString());
     }
 }
 

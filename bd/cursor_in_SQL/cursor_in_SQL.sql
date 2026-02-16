bc8cf35dc3023b2e631bcaa69d16cc77e7f4e5e3
Kudryavceva Irina <kosatkalastochka@gmail.com>
2026-02-16 12:05:17 +0300

    Курсор в SQL

---
+++ bd/cursor_in_SQL/cursor_in_SQL.sql
@@ -0,0 +1,20 @@
+BEGIN;
+DECLARE
+cursor_products SCROLL CURSOR FOR
+SELECT * FROM products;
+
+FETCH LAST FROM cursor_products;
+
+MOVE BACKWARD 4 FROM cursor_products;
+
+FETCH PRIOR FROM cursor_products;
+
+MOVE BACKWARD 7 FROM cursor_products;
+
+FETCH PRIOR FROM cursor_products;
+
+MOVE BACKWARD 4 FROM cursor_products;
+
+FETCH PRIOR FROM cursor_products;
+
+FETCH PRIOR FROM cursor_products;

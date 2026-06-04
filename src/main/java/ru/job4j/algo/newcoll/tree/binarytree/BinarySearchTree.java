f478675abdb4feed0db0ab055426b9900f819404
Kudryavceva Irina <kosatkalastochka@gmail.com>
2026-06-04 21:36:22 +0300

    Бинарное дерево поиска. Очистка дерева.

--- src/main/java/ru/job4j/algo/newcoll/tree/binarytree/BinarySearchTree.java
+++ src/main/java/ru/job4j/algo/newcoll/tree/binarytree/BinarySearchTree.java
@@ -157,6 +157,22 @@ public class BinarySearchTree<T extends Comparable<T>> {
         return list;
     }
 
+    public void clear() {
+        Node node = root;
+        clear(node);
+        root = null;
+    }
+
+    private void clear(Node first) {
+        if (first != null) {
+            clear(first.left);
+            clear(first.right);
+            first.left = null;
+            first.right = null;
+            first.key = null;
+        }
+    }
+
     public List<T> inPostOrder() {
         List<T> list = new ArrayList<>();
         inPostOrder(root, list);

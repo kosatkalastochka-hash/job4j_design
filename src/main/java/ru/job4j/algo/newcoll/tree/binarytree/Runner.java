4fb7764e36839a03aba3c800e64cce1d68a3120e
Kudryavceva Irina <kosatkalastochka@gmail.com>
2026-06-04 10:43:32 +0300

    Рекурсивный вызов метода

---
+++ src/main/java/ru/job4j/algo/newcoll/tree/binarytree/Runner.java
@@ -0,0 +1,9 @@
+package ru.job4j.algo.newcoll.tree.binarytree;
+
+public class Runner {
+        public static void main(String[] args) {
+            ExampleTree exampleTree = new ExampleTree();
+            System.out.println(exampleTree);
+        }
+}
+
---
+++ src/main/java/ru/job4j/algo/newcoll/tree/binarytree/VisualNode.java
@@ -0,0 +1,9 @@
+package ru.job4j.algo.newcoll.tree.binarytree;
+
+public interface VisualNode {
+    VisualNode getLeft();
+
+    VisualNode getRight();
+
+    String getText();
+}
